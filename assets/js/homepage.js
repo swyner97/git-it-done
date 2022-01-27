d90fbc4e48bbd3706dc5329e9f17c6d97c679923
swyner97 <swyner97@gmail.com>
2022-01-26 17:09:37 -0800

    display API response data

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,84 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-  };
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var reposSearchTerm = document.querySelector("#repo-search-term");
 
-getUserRepos();
+var formSubmitHandler = function(event) {
+  // prevent page from refreshing
+  event.preventDefault();
+
+  // get value from input element
+  var username = nameInputEl.value.trim();
+
+  if (username) {
+    getUserRepos(username);
+
+    // clear old content
+    repoContainerEl.textContent = '';
+    nameInputEl.value = '';
+  } else {
+    alert('Please enter a GitHub username');
+  }
+};
+
+var getUserRepos = function(user) {
+  // format the github api url
+  var apiUrl = 'https://api.github.com/users/' + user + '/repos';
+
+  // make a get request to url
+  fetch(apiUrl)
+    .then(function(response) {
+      // request was successful
+      if (response.ok) {
+        response.json().then(function(data) {
+          displayRepos(data, user);
+        });
+        
+      } else {
+        alert('Error: GitHub User Not Found' + response.statusText);
+      }
+    })
+    .catch(function(error) {
+      // Notice this `.catch()` getting chained onto the end of the `.then()` method
+      alert('Unable to connect to GitHub');
+    });
+};
+
+var displayRepos = function (repos, searchTerm) {
+  // check if api returned any repos 
+
+  if (repos.length === 0) {
+    repoContainerEl.textContent = "no repositories found.";
+    return;
+  }
+
+  console.log(repos);
+  console.log(searchTerm);
+
+  // clear old content 
+  repoContainerEl.textContent = "";
+  reposSearchTerm.textContent = searchTerm;
+
+  // loop over repos
+  for (var i = 0; i < repos.length; i++) {
+    // format repo name
+    var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+    // create a container for each repo
+    var repoEl = document.createElement("div");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+    // create a span element to hold repository name
+    var titleEl = document.createElement("span");
+    titleEl.textContent = repoName;
+
+    // append to container
+    repoEl.appendChild(titleEl);
+
+    // append container to the dom
+    repoContainerEl.appendChild(repoEl);
+  };
+};
+
+
+userFormEl.addEventListener('submit', formSubmitHandler);
